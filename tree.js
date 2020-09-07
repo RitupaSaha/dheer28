2677602013bd6adeb267035e1fc9ad9212691b59
RitupaSaha <ritup.saha@gmail.com>
2020-09-08 00:33:45 +0530

    changed and corrected ur files

--- tree.js
+++ tree.js
@@ -1,26 +1,33 @@
-class tree{
-constructor(x,y,width,height){
-var options={
-isStatic:true,
-friction:1,
-restitution:0
-}
-this.x = x;
-this.y = y; 
-this.width = width;
-this.height = height;
-this.image = loadImage("Plucking mangoes/tree.png"); 
-this.body = Bodies.rectangle(x, y,width,height,options); 
-World.add(world, this.body);
-}
-display(){ 
-var pos=this.body.position;
-push();
-translate(pos.x,pos.y);
-//pos.x= mouseX; 
-//pos.y = mouseY; 
-imageMode(CENTER); 
-image(this.image,0,0,this.r,this.r);
-pop();
-}
+class tree
+{
+	constructor(x,y)
+	{
+		this.x=x;
+		this.y=y;
+		this.dustbinWidth=450;
+		this.dustbinHeight=600;
+		this.wallThickness=10;
+		
+		this.image=loadImage("tree.png")
+		this.bottomBody=Bodies.rectangle(this.x, this.y, this.dustbinWidth, this.wallThickness, {isStatic:true})
+		this.leftWallBody=Bodies.rectangle(0, this.y-this.dustbinHeight/2, this.wallThickness, this.dustbinHeight, {isStatic:false})
+		this.rightWallBody=Bodies.rectangle(this.x+this.dustbinWidth/2, this.y-this.dustbinHeight/2, this.wallThickness, this.dustbinHeight, {isStatic:false})
+		
+		World.add(world, this.bottomBody)
+		World.add(world, this.leftWallBody)
+		World.add(world, this.rightWallBody);
+
+	}
+	display()
+	{
+			var posBottom=this.bottomBody.position;
+			push()
+			translate(posBottom.x, posBottom.y+10);
+			fill(255)
+			imageMode(CENTER);
+			image(this.image, 0,-this.dustbinHeight/2,this.dustbinWidth, this.dustbinHeight)
+			pop()
+			
+	}
+
 }
